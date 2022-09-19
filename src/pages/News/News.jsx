eef97cde3d5c829406a06e1ffc07e673fcbc4360
aros0918 <naomifujimura918@gmail.com>
2022-09-19 11:04:03 +0700

    crypto news

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import { Avatar, Card, Col, Row, Select, Typography } from "antd";
+import moment from "moment/moment";
+import React, { useState } from "react";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+import { useGetCryptosNewsQuery } from "../../services/cryptoNewsApi";
 
-const News = () => {
-  return <div>News</div>;
+const demoImage = `http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg`;
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data: cryptoNews } = useGetCryptosNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  const { data: cryptosList } = useGetCryptosQuery(100);
+
+  console.log(cryptoNews);
+
+  if (!cryptoNews?.value) return "Loading...";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowercase().indexOf(input.toLocaleLowerCase()) >
+              0
+            }
+          >
+            <Option value="Cryptocurrency">Cryptocurrency</Option>
+            {cryptosList &&
+              cryptosList?.data?.coins.map((coin) => (
+                <Option value={coin.name}>{coin.name}</Option>
+              ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews &&
+        cryptoNews.value.map((news, i) => (
+          <Col xs={24} sm={12}>
+            <Card hoverable className="news-card">
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Title className="news-title" level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    style={{ maxWidth: "200px", maxHeight: "100px" }}
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt="News"
+                  />
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+                <div className="provider-container">
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                      alt="News"
+                    />
+                    <Text className="provider-name">
+                      {news.provider[0]?.name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf(`ss`).fromNow()}
+                  </Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+    </Row>
+  );
 };
 
 export default News;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "e9b554d91amsh382076b6c3a8a46p152b2ejsndbc63c85e132",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = `https://bing-news-search1.p.rapidapi.com`;
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptosNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptosNewsQuery } = cryptoNewsApi;
