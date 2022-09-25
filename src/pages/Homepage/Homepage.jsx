1e1d97b832cb7a40641a682c02fc41273c487fe3
aros0918 <naomifujimura918@gmail.com>
2022-09-25 18:19:58 +0700

    refactor

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -14,49 +14,47 @@ const Homepage = () => {
 
   if (isFetching) return <Loader />;
 
+  console.log(globalStats);
+
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
-      {globalStats?.total &&
-        globalStats?.totalExchanges &&
-        globalStats?.totalMarketCap &&
-        globalStats?.total24hVolume &&
-        globalStats?.totalMarkets && (
-          <Row>
-            <Col span={12}>
-              <Statistic
-                title="Total Cryptocurrencies"
-                value={globalStats?.total}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Total Exchanges"
-                value={millify(globalStats.totalExchanges)}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Total Market Cap"
-                value={millify(globalStats.totalMarketCap)}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Total 24h Volume"
-                value={millify(globalStats.total24hVolume)}
-              />
-            </Col>
-            <Col span={12}>
-              <Statistic
-                title="Total Markets"
-                value={millify(globalStats.totalMarkets)}
-              />
-            </Col>
-          </Row>
-        )}
+      {globalStats && (
+        <Row>
+          <Col span={12}>
+            <Statistic
+              title="Total Cryptocurrencies"
+              value={globalStats?.total}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Exchanges"
+              value={millify(globalStats.totalExchanges)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Market Cap"
+              value={millify(globalStats.totalMarketCap)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total 24h Volume"
+              value={millify(globalStats.total24hVolume)}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Total Markets"
+              value={millify(globalStats.totalMarkets)}
+            />
+          </Col>
+        </Row>
+      )}
 
       <div className="home-heading-container">
         <Title level={2} className="home-title">
